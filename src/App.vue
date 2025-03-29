161ba29bf221e19ecb6720416fa175dda9fd1123
Mykhailo Kushchov <mkuschov@gmail.com>
2025-03-29 15:32:03 -0400

    Init vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref('Hello World!');
+const newTask = ref('');
+</script>
+
+<template>
+    <main>
+        <h1>{{ message }}</h1>
+        <form>
+            <label>New Task
+                <input v-model="newTask" name="newTask">
+            </label>
+        </form>
+        <h3>{{ newTask }}</h3>
+    </main>
+</template>
+
+<style scoped>
+main {
+    max-width: 800px;
+    margin: 1rem auto;
+}
+</style>
